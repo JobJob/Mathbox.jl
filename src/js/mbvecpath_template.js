31a4b11e54f2a7fb5bb8f6878439ab4b29a4e65d
Joel Mason <jobba1@hotmail.com>
2016-02-25 14:12:49 +0200

    Allow holding at end of animation

--- src/js/mbvecpath_template.js
+++ src/js/mbvecpath_template.js
@@ -4,12 +4,15 @@ divisions = span
 
 var d1dims = 3
 var mbjl_loop = true
-var vec_trails = 1
+var vec_trails = 10
+var end_wait_cycles = 4
+var vecs_per_sec = 1
+
+var end_cycles_waited = 0
 var firstidx = 0
 var lastidx = 0
-var last_valid_baseidx = data1.length - 6
+var last_valid_baseidx = data1.length - 6 //need 2*3D points for a vector
 var prev_incrt = 0
-var vecs_per_sec = 1
 var vecs_delta_secs = 1/vecs_per_sec
 var incr_count = 0
 view
@@ -18,8 +21,13 @@ view
     height: dcols1,
     expr: function (emit, x, y, t, d) {
       var idx = x + y*drows1 // baseidx(x,y,t)
-      if (idx <= last_valid_baseidx && idx >= firstidx && idx <= lastidx){
-        if (vec_trails > 0 && incr_count > vec_trails && idx == firstidx){
+      if (idx <= last_valid_baseidx
+        && idx >= firstidx && idx <= lastidx){ //controls which vectors in path are shown
+        var lerpfraction = (t - prev_incrt)/vecs_delta_secs
+        if (vec_trails > 0 && incr_count > vec_trails //interp/slide trailing bases in
+          && idx == firstidx //only the last visible base needs ot be interpolated
+          && end_cycles_waited == 0 //don't interpolate if holding/pausing at end of path anim
+          && idx + vec_trails*drows1 < last_valid_baseidx ){ //don't interpolate if it's the last base index (since we might be holding at the end)
           emit(
             mbbasic_lerp(data1[idx],   data1[idx+3], lerpfraction),
             mbbasic_lerp(data1[idx+1], data1[idx+4], lerpfraction),
@@ -28,8 +36,7 @@ view
         }else{
           emit(data1[idx], data1[idx+1], data1[idx+2]) //bases
         }
-        lerpfraction = (t - prev_incrt)/vecs_delta_secs
-        if (idx == lastidx){
+        if (idx == lastidx && end_cycles_waited == 0){
           emit(
             mbbasic_lerp(data1[idx],   data1[idx+3], lerpfraction),
             mbbasic_lerp(data1[idx+1], data1[idx+4], lerpfraction),
@@ -42,11 +49,24 @@ view
       if (t - prev_incrt >= vecs_delta_secs){
         lastidx += d1dims
         incr_count += 1
-        if(vec_trails >= 0 && incr_count > vec_trails) firstidx += d1dims
+        if (vec_trails >= 0 && incr_count > vec_trails){
+          firstidx += d1dims
+        }
         prev_incrt = t
         if (mbjl_loop && lastidx > last_valid_baseidx){
-          firstidx = lastidx = 0
-          incr_count = 0
+          if (end_cycles_waited >= end_wait_cycles){
+            firstidx = lastidx = 0
+            incr_count = 0
+            end_cycles_waited = 0
+          }else{
+            //note after this is set we still wait a whole vecs_delta_secs
+            //till a potential restart
+            end_cycles_waited += 1
+            lastidx -= d1dims
+            if (vec_trails >= 0 && incr_count > vec_trails){
+              firstidx -= d1dims
+            }
+          }
         }
       }
     },
